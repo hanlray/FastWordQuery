574c5a693f3b6d256459934a4e60010310bfe3c9
hanlray <hanlray@gmail.com>
2020-11-16 19:53:04 +0800

    improved sense lookup

--- addons21/fastwq/service/dict/LDOCE5.py
+++ addons21/fastwq/service/dict/LDOCE5.py
@@ -3,6 +3,7 @@ import os
 import re
 import random
 import sys
+from enum import Enum
 from ..base import *
 #from BeautifulSoup import BeautifulSoup
 from bs4 import BeautifulSoup
@@ -22,6 +23,11 @@ DEF_CN_PATTERN = re.compile(
 # u'E:\\BaiduYunDownload\\mdx\\L6mp3.mdx'
 DICT_PATH = u'D:\\dict\\LDOCE5++ V 2-15\\LDOCE5++ V 2-15.mdx'
 
+
+class DefinitionLang(Enum):
+    ENG = 1
+    CHN = 2
+    BILINGUAL = 3
 
 @register([u'本地词典-LDOCE5++', u'MDX-LDOCE5++'])
 class Ldoce5plus(MdxService):
@@ -65,55 +71,110 @@ class Ldoce5plus(MdxService):
         return self._fld_voice(self.get_html(), 'us')
 
     @export('DEF_CN_1_part_1_sense_1_sentence')
-    def fld_definition_cn(self):
-        return self._range_definition_cn(1, 1, 1)
+    def fld_cn_111(self):
+        return self._range_definition_cn(DefinitionLang.CHN, 1, 1, 1)
 
     @export('DEF_CN_1_part_1_sense_1_sentence_audio')
     def fld_cn_111a(self):
-        sys.stdout.write(self.get_html())
-        return self._range_definition_cn(1, 1, 1, True)
+        #sys.stdout.write(self.get_html())
+        return self._range_definition_cn(DefinitionLang.CHN, 1, 1, 1, True)   
 
-    def _range_definition_cn(self, part_count, sense_count, sentence_count, with_audio=False):
+    @export('DEF_CN_1_part_1_sense_2_sentence')
+    def fld_cn_112(self):
+        return self._format_tab(self._range_definition_cn(DefinitionLang.CHN, 1, 1, 2))
+                
+    @export('DEF_CN_1_part_1_sense_2_sentence_audio')
+    def fld_cn_112a(self):
+        #sys.stdout.write(self.get_html())
+        return self._range_definition_cn(DefinitionLang.CHN, 1, 1, 2, True)     
+
+    @export('DEF_CN_2_part_1_sense_2_sentence')
+    def fld_cn_212(self):
+        return self._css(self._format_tab(self._range_definition_cn(DefinitionLang.CHN, 2, 1, 2)))
+
+    @export('DEF_CN_2_part_1_sense_2_sentence_audio)')
+    def fld_cn_212a(self):
+        return self._css(self._format_tab(self._range_definition_cn(DefinitionLang.CHN, 2, 1, 2, True)))
+
+    @export('DEF_EN_2_part_1_sense_2_sentence_audio)')
+    def fld_en_212a(self):
+        return self._css(self._format_tab(self._range_definition(DefinitionLang.ENG, 2, 1, 2, True)))
+
+    def _format_tab(self, entries):
+        result = '<div class="tabs">'
+        for i, entry_key in enumerate(entries):
+            result += '''
+            <div class="tab">
+                <input type="radio" id="{0}" name="tab-group-1"{2}>
+                <label for="{0}">{0}</label>
+                <div class="content">{1}</div>
+            </div>
+            '''.format(entry_key, entries[entry_key], ' checked' if i==0 else '')
+        result += '</div>'
+
+        return result
+
+    def _range_definition(self, lang, part_count, sense_count, sentence_count, with_audio=False):
+        #sys.stdout.write(self.get_html())
         soup = BeautifulSoup(self.get_html(), 'html.parser')
         entries = soup.find_all("div", class_="dictentry")
-        result = ''
+        
         part_index = 0
+        result = {}
         for entry in entries:
             if 'bussdict' in entry['class']:
                 continue
-            
-            head = entry.find('span', class_='frequent Head')
-            if head:
-                cx = head.find('span', class_='lm5pp_POS')
-                result += cx.get_text() + '<br>'
+        
+            #head = entry.find('span', class_='frequent Head')
+            part_elem = entry.select_one('.Head .lm5pp_POS')
+            if part_elem:
+                part = part_elem.get_text()
+
+                senses_text = ''
                 senses = entry.find_all('div', class_='Sense')
                 for j, sense in enumerate(senses):
-                    def_cn = sense.select_one('.DEF .cn_txt')
-                    result += def_cn.get_text()
-                    result += self._range_sentence(sense, sentence_count, with_audio)
+                    if lang == DefinitionLang.ENG:
+                        sense_head = sense.select_one('.DEF').get_text()
+                    elif lang == DefinitionLang.CHN:
+                        sense_head = sense.select_one('.DEF .cn_txt').get_text()
+                    elif lang == DefinitionLang.BILINGUAL:
+                        sense_head = sense.select_one('.DEF').get_text() + sense.select_one('.DEF .cn_txt').get_text()
+
+                    senses_text += sense_head
+                    senses_text += self._range_sentence(lang, sense, sentence_count, with_audio)
                     if j == (sense_count-1):
                         break
+
+                result[part] = senses_text
 
                 part_index += 1
                 if part_index == part_count:
                     break
-
+        #sys.stdout.write(str(result))
         return result            
 
-    def _range_sentence(self, sense_elem, sentence_count, with_audio):
+    def _range_sentence(self, lang, sense_elem, sentence_count, with_audio):
         if sentence_count <= 0:
             return ''
 
-        result = '<br>'
+        result = ''
         examples = sense_elem.select('div.EXAMPLE')
         for i, example in enumerate(examples):
-            result += example.select_one('.english').get_text()
+            english_elem = example.select_one('.english')
+            if lang == DefinitionLang.ENG:
+                english_elem.select_one('.cn_txt').decompose()
+            
+            result += '<br>' + english_elem.get_text()
             if with_audio:
                 audio_href = example.select_one('.speaker')['href']
                 result += self._fld_audio(audio_href.replace('sound:/', ''))
             if i == (sentence_count-1):
                 break
         return result
+
+    @with_styles(cssfile='_common.css')
+    def _css(self, val):
+        return val
 
     @export('All examples with audios')
     def fld_sentence_audio(self):
